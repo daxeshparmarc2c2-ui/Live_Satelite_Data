04490924c8f45ef5676143fef8762cb6c1390ff4
daxeshparmarc2c2-ui <daxeshparmarc2c2@gmail.com>
2025-11-30 22:32:00 -0500

    Update groups.py

--- groups.py
+++ groups.py
@@ -18,5 +18,5 @@ GP_GROUPS = {
     "cosmos-1408-debris": "https://celestrak.org/NORAD/elements/gp.php?GROUP=cosmos-1408-debris&FORMAT=json",
     "fengyun-1c-debris": "https://celestrak.org/NORAD/elements/gp.php?GROUP=fengyun-1c-debris&FORMAT=json",
     "iridium-33-debris": "https://celestrak.org/NORAD/elements/gp.php?GROUP=iridium-33-debris&FORMAT=json",
-    "cosmos-2251-debris": "https://celestrak.org/NORAD/elements/gp.php?GROUP=cosmos-2251-debris&FORMAT=json"
+    "cosmos-2251-debris": "https://celestrak.org/NORAD/elements/gp.php?GROUP=cosmos-2251-debris&FORMAT=json",
 }
